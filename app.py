f1d521adaef16a1a8c87985dbb1aa4237f959b0b
Av1352 <66872073+Av1352@users.noreply.github.com>
2021-07-12 15:01:37 +0530

    first print(count)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -8,12 +8,17 @@ db = cluster["test"]
 collection = db["test"]
 count = collection.count_documents({})
 print(count)
-# @app.route('/')
-# def index():
-#     return collection
-# @app.route("/users", methods=["POST"])
-# def create_user():
-#     return collection
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html', count=count)
+
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    return collection
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
