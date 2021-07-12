5454ee9a371b54aa95fa5670baabb907d4db67cb
Av1352 <66872073+Av1352@users.noreply.github.com>
2021-07-12 20:50:18 +0530

    login validation with signout

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, url_for, redirect, session
 from pymongo import MongoClient
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
 app_root = os.path.abspath(os.path.dirname(__file__))
 
 # <-------- MONGO CONNECTION -------->
@@ -37,16 +38,11 @@ def signup():
         return redirect(url_for("logged_in"))
 
     if request.method == "POST":
-        keys_signup = ["name", "email", "password"]
+        # keys_signup = ["name", "email", "password"]
         name = request.form.get('name')
         email = request.form.get('email')
         password = request.form.get('pass')
         re_pass = request.form.get('re_pass')
-        # values = []
-        # values.extend((name, email, password))
-        # dict_signup = to_dictionary(keys_signup, values)
-        # print(dict_signup)
-        # collection_signup.insert_one(dict_signup)
         # if found in database showcase that it's found
         user_found = collection_signup.find_one({"name": name})
         email_found = collection_signup.find_one({"email": email})
@@ -62,7 +58,7 @@ def signup():
         else:
             # hash the password and encode it
             hashed = bcrypt.hashpw(re_pass.encode('utf-8'), bcrypt.gensalt())
-            # assing them in a dictionary in key value pairs
+            # inserting them in a dictionary in key value pairs
             user_input = {'name': name, 'email': email, 'password': hashed}
             # insert it in the record collection
             collection_signup.insert_one(user_input)
@@ -79,13 +75,50 @@ def signup():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    message = 'Please login to your account'
+    if "email" in session:
+        return redirect(url_for("logged_in"))
+
     if request.method == "POST":
-        username = request.form.get('your_name')
+        email = request.form.get('email')
         password = request.form.get('your_pass')
-        print(username, password)
-        return render_template("login.html")
-    if request.method == "GET":
-        return render_template("login.html")
+
+        # check if email exists in database
+        email_found = collection_signup.find_one({"email": email})
+        if email_found:
+            email_val = email_found['email']
+            passwordcheck = email_found['password']
+            # encode the password and check if it matches
+            if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
+                session["email"] = email_val
+                return redirect(url_for('logged_in'))
+            else:
+                if "email" in session:
+                    return redirect(url_for("logged_in"))
+                message = 'Wrong password'
+                return render_template('login.html', message=message)
+        else:
+            message = 'Email not found'
+            return render_template('login.html', message=message)
+    return render_template('login.html', message=message)
+
+
+@app.route('/logged_in')
+def logged_in():
+    if "email" in session:
+        email = session["email"]
+        return render_template('logged_in.html', email=email)
+    else:
+        return redirect(url_for("login"))
+
+
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    if "email" in session:
+        session.pop("email", None)
+        return render_template("signout.html")
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
